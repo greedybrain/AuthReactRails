d996e0db886b56e4d845229eb5b7194a9187c376
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-06-19 21:55:29 -0400

    created dashboard component

--- frontend-auth/src/component/Dashboard.jsx
+++ frontend-auth/src/component/Dashboard.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const Dashboard = () => {
+const Dashboard = ({loggedInStatus}) => {
      return ( 
           <div>
                <h1>Dashboard</h1>
+               <h1>Status: {loggedInStatus}</h1>
           </div>
      );
 }
