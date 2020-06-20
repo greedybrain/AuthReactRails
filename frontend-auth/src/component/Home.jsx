1b61c2fa8926b43c4e8c0c7716e6de2fcbdb8d03
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-06-19 21:55:54 -0400

    created handleSuccessfulAuth method in home comp

--- frontend-auth/src/component/Home.jsx
+++ frontend-auth/src/component/Home.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import Registration from './auth/Registration';
+import Login from './auth/Login';
+import axios from 'axios'
 
 class Home extends Component {
+
+     handleSuccessfulAuth = data => {
+          this.props.handleLogin(data)
+          this.props.history.push('/dashboard')
+     }
+
      render() {
           return (
                <div>
+                    <button onClick={this.props.handleLogout}>Logout</button>
                     <h1>Home</h1>
+                    <h1>Status: {this.props.loggedInStatus}</h1>
+                    <Registration handleSuccessfulAuth={this.handleSuccessfulAuth} />
+                    <Login handleSuccessfulAuth={this.handleSuccessfulAuth} />
                </div>
           );
      }
